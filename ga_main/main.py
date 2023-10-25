33cd7c76157162fc15445386c1b95cd344ca8e32
Fa7adbina <88969472+Fa7adbina@users.noreply.github.com>
2023-10-25 21:14:45 +0300

    Update main.py

--- ga_main/main.py
+++ ga_main/main.py
@@ -4,16 +4,6 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# استدعاء الصفحة الترحيبيه *********************************
-@app.route("/")
-def root():
-    return render_template("welcome.html")
-
-# هذا استدعاء صفحة الموديل ********************************
-@app.route('/main')
-def main():
-    return render_template("index.html")
-
 @app.route('/backend', methods=['POST'])
 def backend():
     # Receive the user input and language from the frontend
@@ -26,11 +16,4 @@ def backend():
 
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run()
