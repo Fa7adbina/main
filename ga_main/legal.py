171b50b2ef569ac6fae396451c4255dc8cce502e
Fa7adbina <88969472+Fa7adbina@users.noreply.github.com>
2023-10-25 21:14:03 +0300

    Update legal.py

--- ga_main/legal.py
+++ ga_main/legal.py
@@ -1,8 +1,7 @@
 def Legal_Researcher(input_variable):
     
-  # input_variable_1 = input_variable+" . "+language
-  # input_variable_1= agent.run(input_variable)
-  Output_For_Function= input_variable_1 + input_variable
+
+  Output_For_Function= input_variable + input_variable
 
   return Output_For_Function
 
